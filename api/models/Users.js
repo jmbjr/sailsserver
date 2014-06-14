e8c06786bf756affa24aca9875a0c9d81d47b5b6
jmbjr <boylejm@gmail.com>
2014-06-14 16:16:26 -0400

    initial commit

---
+++ api/models/Users.js
@@ -0,0 +1,15 @@
+/**
+ * Users
+ *
+ * @module      :: Model
+ * @description :: A short summary of how this model works and what it represents.
+ * @docs		:: http://sailsjs.org/#!documentation/models
+ */
+
+//Users Model
+module.exports = {
+    attributes  : {
+         username: 'STRING',
+         password: 'STRING'     
+    }
+};
