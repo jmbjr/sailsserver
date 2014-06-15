68cd22fbe8e53db9e53cc5d615e6cee0d3951fcd
jmbjr <boylejm@gmail.com>
2014-06-14 21:33:32 -0400

    replaced with ichabodcole version

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -1,77 +1,93 @@
 /**
- * MainController
- *
- * @module      :: Controller
- * @description	:: A set of functions called `actions`.
- *
- *                 Actions contain code telling Sails how to respond to a certain type of request.
- *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
- *
- *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
- *                 and/or override them with custom routes (`config/routes.js`)
- *
- *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
- *
- * @docs        :: http://sailsjs.org/#!documentation/controllers
- */
+* MainController
+*
+* @module :: Controller
+* @description :: Contains logic for handling requests.
+*/
 
-var MainController = {
-    index: function (req, res) {
-        res.view();         
-    },
-    signup: function (req, res) {
-        var username = req.param("username");
-        var password = req.param("password");
-         
-        Users.findByUsername(username).done(function(err, usr){
-            if (err) {
-                res.send(500, { error: "DB Error" });
-            } else if (usr) {
-                res.send(400, {error: "Username already Taken"});
-            } else {
-                var hasher = require("password-hash");
-                password = hasher.generate(password);
-                 
-                Users.create({username: username, password: password}).done(function(error, user) {
-                    if (error) {
-                        res.send(500, {error: "DB Error"});
-                    } else {
-                        req.session.user = user;
-                        res.send(user);
-                    }
-                });
-            }
+module.exports = {
+
+  index: function (req, res) {
+    res.view();
+  },
+
+  signup: function (req, res) {
+    var username = req.param('username');
+    var password = req.param('password');
+    // Users.findByUsername(username)...
+    // In v0.9.0 the find method returns an empty array when no results are found
+    // when only one result is needed use findOne.
+    Users.findOneByUsername(username)
+    .done(function signupfindUser(err, usr){
+      if (err) {
+        // We set an error header here,
+        // which we access in the views an display in the alert call.
+        res.set('error', 'DB Error');
+        // The error object sent below is converted to JSON
+        res.send(500, { error: "DB Error" });
+      } else if (usr) {
+        // Set the error header
+        res.set('error', 'Username already Taken');
+        res.send(400, { error: "Username already Taken"});
+      } else {
+        var hasher = require("password-hash");
+        password = hasher.generate(password);
+
+        Users.create({ username: username, password: password })
+        .done(function signupCreatUser(error, user) {
+          if (error) {
+            // Set the error header
+            res.set('error', 'DB Error');
+            res.send(500, { error: "DB Error" });
+          } else {
+            req.session.user = user;
+            res.send(user);
+          }
         });
-    },
-    login: function (req, res) {
-        var username = req.param("username");
-        var password = req.param("password");
-     
-        Users.findByUsername(username).done(function(err, usr) {
-            if (err) {
-                res.send(500, { error: "DB Error" });
-            } else {
-                if (usr) {
-                    var hasher = require("password-hash");
-                    if (hasher.verify(password, usr.password)) {
-                        req.session.user = usr;
-                        res.send(usr);
-                    } else {
-                        res.send(400, { error: "Wrong Password" });
-                    }
-                } else {
-                    res.send(404, { error: "User not Found" });
-                }
-            }
-        });         
-    },
-    chat: function (req, res) {
-        if (req.session.user) {
-            res.view({username: req.session.user.username});
-        } else {
-            res.redirect('/');
-        }         
-    }
-};
-module.exports = MainController;
+      }
+    });
+  },
 
+  login: function (req, res) {
+    var username = req.param('username');
+    var password = req.param('password');
+
+    // Users.findByUsername(username)...
+    // In v0.9.0 the find method returns an empty array when no results are found
+    // when only one result is needed use findOne.
+    Users.findOneByUsername(username)
+    .done(function loginfindUser(err, usr){
+      if (err) {
+        // We set an error header here,
+        // which we access in the views an display in the alert call.
+        res.set('error', 'DB Error');
+        // The error object sent below is converted to JSON
+        res.send(500, { error: "DB Error" });
+      } else {
+        if (usr) {
+          var hasher = require("password-hash");
+          if (hasher.verify(password, usr.password)) {
+            req.session.user = usr;
+            res.send(usr);
+          } else {
+            // Set the error header
+            res.set('error', 'Wrong Password');
+            res.send(400, { error: "Wrong Password" });
+          }
+        } else {
+          res.set('error', 'User not Found');
+          res.send(404, { error: "User not Found"});
+        }
+      }
+    });
+  },
+
+  chat: function (req, res) {
+    if (req.session.user) {
+      res.view({ username: req.session.user.username });
+    } else {
+      res.redirect('/');
+    }
+  }
+
+};
